dc4c7ede59f166ec8869dcd5db746379f4e3630a
gilmar.silva <gilmar.silva@mpgo.mp.br>
2020-09-18 15:19:01 -0300

    projeto criado

---
+++ app.js
@@ -0,0 +1,14 @@
+angular.module('AppAula6', []).controller('aula6Controler',function ($scope, $http) {
+    $scope.titulo = "titulo da pagina"
+    
+
+    $scope.carregarPessoas = function (){
+        $http({
+            url: 'pessoa.json',
+            method: 'GET'
+        }).then(function(resposta){
+            $scope.listaPessoas = resposta.data;
+        })
+    };
+
+});
